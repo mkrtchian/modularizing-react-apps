77cd2d89bbbd6a1c1ea2cd66a017ce8a476e9b87
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-02 00:29:08 +0200

    feat(payment): add donation feature

--- app/components/Payment.tsx
+++ app/components/Payment.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMemo, useState } from "react";
 import { FetchPort } from "../adapters/fetchAdapter";
 import { usePaymentMethods } from "../hooks/usePaymentMethods";
 import { PaymentMethods } from "./PaymentMethods";
@@ -12,11 +13,34 @@ export const Payment = ({
   fetchAdapter: FetchPort;
 }) => {
   const { paymentMethods } = usePaymentMethods(fetchAdapter);
+  const [agreeToDonate, setAgreeToDonate] = useState<boolean>(false);
+  const { total, tip } = useMemo(
+    () => ({
+      total: agreeToDonate ? Math.floor(amount + 1) : amount,
+      tip: parseFloat((Math.floor(amount + 1) - amount).toPrecision(10)),
+    }),
+    [amount, agreeToDonate],
+  );
+  const handleChange = () => setAgreeToDonate(!agreeToDonate);
   return (
     <div>
       <h3>Payment</h3>
       <PaymentMethods options={paymentMethods} />
-      <button>${amount}</button>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            onChange={handleChange}
+            checked={agreeToDonate}
+          />
+          <p>
+            {agreeToDonate
+              ? "Thanks for your donation."
+              : `I would like to donate $${tip} to charity.`}
+          </p>
+        </label>
+      </div>
+      <button>${total}</button>
     </div>
   );
 };
