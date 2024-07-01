03daec22842e333e1921fd12283601c0084ab1c2
Roman Mkrtchian <mkrtchia@gmail.com>
2024-07-01 22:52:08 +0200

    refactor: split view and non view code

--- app/Payment.tsx
+++ app/Payment.tsx
@@ -17,29 +17,7 @@ const remotePaymentMethodsSchema = z.array(
 type PaymentProps = { amount: number };
 
 export const Payment = ({ amount }: PaymentProps) => {
-  const [paymentMethods, setPaymentMethods] = useState<LocalPaymentMethod[]>(
-    [],
-  );
-
-  useEffect(() => {
-    const fetchPaymentMethods = async () => {
-      const url = "https://online-ordering.com/api/payment-methods";
-      const response = await fetch(url);
-      const methods = remotePaymentMethodsSchema.parse(await response.json());
-      if (methods.length > 0) {
-        const extended: LocalPaymentMethod[] = methods.map((method) => ({
-          provider: method.name,
-          label: `Pay with ${method.name}`,
-        }));
-        extended.push({ provider: "cash", label: "Pay in cash" });
-        setPaymentMethods(extended);
-      } else {
-        setPaymentMethods([]);
-      }
-    };
-
-    void fetchPaymentMethods();
-  }, []);
+  const { paymentMethods } = usePaymentMethods();
 
   return (
     <div>
@@ -61,3 +39,32 @@ export const Payment = ({ amount }: PaymentProps) => {
     </div>
   );
 };
+
+function usePaymentMethods() {
+  const [paymentMethods, setPaymentMethods] = useState<LocalPaymentMethod[]>(
+    [],
+  );
+
+  useEffect(() => {
+    const fetchPaymentMethods = async () => {
+      const url = "https://online-ordering.com/api/payment-methods";
+      const response = await fetch(url);
+      const methods = remotePaymentMethodsSchema.parse(await response.json());
+      if (methods.length > 0) {
+        const extended: LocalPaymentMethod[] = methods.map((method) => ({
+          provider: method.name,
+          label: `Pay with ${method.name}`,
+        }));
+        extended.push({ provider: "cash", label: "Pay in cash" });
+        setPaymentMethods(extended);
+      } else {
+        setPaymentMethods([]);
+      }
+    };
+    void fetchPaymentMethods();
+  }, []);
+
+  return {
+    paymentMethods,
+  };
+}
